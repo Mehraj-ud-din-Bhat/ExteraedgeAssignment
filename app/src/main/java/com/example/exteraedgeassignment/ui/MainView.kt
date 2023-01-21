a00938fd1ab6da98387d8e9f0a5185243d91b0e3
mehraj <mehraj@cdse-it.com>
2023-01-21 14:15:03 +0530

    Completed API and local caching

--- app/src/main/java/com/example/exteraedgeassignment/ui/MainView.kt
+++ app/src/main/java/com/example/exteraedgeassignment/ui/MainView.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.exteraedgeassignment.R
+import com.example.exteraedgeassignment.utils.Resource
 import com.example.exteraedgeassignment.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,9 +14,30 @@ class MainView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainViewModel.rockets.observe(this){
-           if(true)
+        mainViewModel.rockets.observe(this){result->
+           if(result!=null)
            {
+               when (result) {
+                   is Resource.Error -> {
+                       if (result.data.isNullOrEmpty()) {
+                         //
+                       //progressBar.isVisible = false
+                           //errorT.isVisible = true
+                           //errorT.text = result.error?.localizedMessage
+                       }
+                   }
+
+                   is Resource.Loading -> {
+                       if (result.data.isNullOrEmpty()) {
+                          // progressBar.isVisible = true
+                       }
+                   }
+
+                   is Resource.Success -> {
+                       //progressBar.isVisible = false
+                       //errorT.isVisible = false
+                   }
+               }
 
            }
         }
